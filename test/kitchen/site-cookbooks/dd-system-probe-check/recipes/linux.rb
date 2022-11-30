88f67b1195c3149270e74a9e88c2531de6f4bc62
Guy Arbitman <guy20495@gmail.com>
2022-11-30 15:48:31 +0200

    wip

--- test/kitchen/site-cookbooks/dd-system-probe-check/recipes/linux.rb
+++ test/kitchen/site-cookbooks/dd-system-probe-check/recipes/linux.rb
@@ -162,3 +162,45 @@ end
 directory "/tmp/pkgjson" do
   recursive true
 end
+
+if ['oracle'].include?(node[:platform])
+    docker_installation_package 'default' do
+      action :create
+      setup_docker_repo false
+      package_name 'docker-engine'
+      package_options %q|-y|
+    end
+
+    service 'docker' do
+      action [ :enable, :start ]
+    end
+elsif ['amazon'].include?(node[:platform])
+    docker_installation_package 'default' do
+      action :create
+      setup_docker_repo false
+      package_name 'docker'
+      package_options %q|-y|
+    end
+
+    service 'docker' do
+      action [ :enable, :start ]
+    end
+elsif ['ubuntu'].include?(node[:platform])
+    docker_installation_package 'default' do
+      action :create
+      setup_docker_repo false
+      package_name 'docker.io'
+    end
+else
+    docker_service 'default' do
+      action [:create, :start]
+    end
+end
+
+execute 'install docker-compose' do
+  command <<-EOF
+    curl -SL https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s | awk '{print tolower($0)}')-$(uname -m) -o /usr/bin/docker-compose
+    chmod 0755 /usr/bin/docker-compose
+  EOF
+  live_stream true
+end
